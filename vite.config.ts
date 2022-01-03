fbe570207e1f8fc53e0bb30e825fdc063ac29761
kbtaniguchi <t.kiminobu@gmail.com>
2022-01-03 23:36:07 +0900

    github pages への公開設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,21 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve:{
-    alias:{
-      '@' : path.resolve(__dirname, './src')
+    base: '/vue3-every-layout/',
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src')
+        },
     },
-  },
-  plugins: [vue()],
-  server: {
-    host: true
-  }
+    build: {
+        assetsDir: './',
+        outDir: 'docs',
+    },
+    plugins: [vue()],
+    server: {
+        host: true
+    },
 })
